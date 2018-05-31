9d5bd14286d8c32a8396a14402f990e4f5898f06
mucco <mufan0313@163.com>
2018-05-31 11:20:53 +0800

    update

--- test/test.js
+++ test/test.js
@@ -9,4 +9,7 @@ describe('the sum of all numbers between min number and max number', function ()
 	it('[-2, 20] return 207', function () {
 		sum([-2, 20]).should.equal(207)
 	})
+	it('[666, 666666] return 222221889666', function () {
+		sum([666, 666666]).should.equal(222221889666)
+	})
 })
