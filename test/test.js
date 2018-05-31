ac5d5b5d4aeea098311938df8c3904a3d7445fb3
mucco <mufan0313@163.com>
2018-05-31 11:13:02 +0800

    init

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var sum = require('../lib/index')
+//var should = require('should')
+
+
+describe('the sum of all numbers between min number and max number', function () {
+	it('[8, 3] return 33', function () {
+		sum([8, 3]).should.equal(33)
+	})
+	it('[-2, 20] return 207', function () {
+		sum([-2, 20]).should.equal(207)
+	})
+})
